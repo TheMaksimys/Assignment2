919509fc82898d3689efdb076ce941e1c0cb000b
TheMaksimys <99420487+TheMaksimys@users.noreply.github.com>
2022-03-03 15:52:24 +0000

    Update Assignment2.ino
    keep working

--- Assignment2.ino
+++ Assignment2.ino
@@ -2,16 +2,38 @@
 #define analogPin 36
 #define a 0
 #define b 1
+#define z 2
+#define y 3
+#define Button1 22
+#define Signal 23
+#define Error 21
+#define Pulse 20
 
 unsigned  long start;
 unsigned long finish;
 unsigned  int task;
+unsigned int tyme;
+
 unsigned int val;
-char counter=0;
+unsigned int counter=0;
 char c=0;
-char i;
+char pock;
+unsigned int reading;
+unsigned int lastReading;
+unsigned int haldCycle;
+float frequency;
+char button;
+char error_code;
 unsigned int dato[4];
 unsigned int avg;
+
+
+unsigned int strt;
+unsigned int fnsh;
+unsigned int toime;
+
+unsigned int beginning;
+unsigned int ending;
 
 
 void setup() {
@@ -19,16 +41,36 @@ void setup() {
    Serial.begin(9600);
      //   adc1_config_width(width);
       //  adc1_config_channel_atten(channel, atten);
-
-   
+      pinMode(Button1, INPUT);
+      pinMode(Pulse, INPUT);
+      pinMode(Signal, OUTPUT);
+        pinMode(Error, OUTPUT);
+      pinMode(Button1, INPUT_PULLDOWN);
+   pinMode(Pulse, INPUT_PULLDOWN);
 }
 
 void loop() {
 
+start=micros();
+ counter++;
+ if(counter>600) {counter=0;} // Clear counter after 5 seconds (0.2 Hz)
+ Serial.print("Execution time:");
+ Serial.print(task,DEC);
+ Serial.print('\n');
+
+
+//Printing what needs to be printed
+
+
+
+
+
+ 
 //Reading potentiometer
     if(((counter+a)%5)==0) // 24 Hz cycle
     {
        val = analogRead(analogPin); 
+        Serial.print("Analog read:");
         Serial.print(val,DEC);
         Serial.print('\n');
     }
@@ -42,21 +84,80 @@ void loop() {
    
    avg=dato[0]+dato[1]+dato[2]+dato[3];
    avg=avg>>2;
+   Serial.print("Average value:");
         Serial.print(avg,DEC);
         Serial.print('\n');
         
     }
 
+//Reading pushbutton
+    if(((counter+z)%24)==0) // 5 Hz cycle
+    {
+       button= digitalRead(Button1);
+       if(button==1) { digitalWrite(Signal,HIGH);}
+       else { digitalWrite(Signal,LOW);}
+        Serial.print("Button state:");
+        Serial.print(button,DEC);
+        Serial.print('\n');
+    }
+ //Do nothing thousand times
+    if(((counter+z)%12)==0) // 10 Hz cycle
+    {
+       for(char i=0; i++; i<250)
+       {
+          for(char j=0; i++; i<4)
+          {
+          __asm__ __volatile__ ("nop");
+          }
+       }
+    }
+// perform check
+      if(((counter+y)%40)==0) // 3 Hz cycle
+      {
+      if(avg > 2047){error_code = 1;}
+      else{ error_code = 0;}
+      }
+
+ // vusualise check result
+      if(((counter+y)%40)==0) // 3 Hz cycle
+      {
+      if(error_code==1){digitalWrite(Error,HIGH);}
+      else{digitalWrite(Error,LOW);}
+      }
 
 
+//evaluation of frequency
+ if(((counter)%120)==0) // 1 Hz cycle
+ {
+  pock=0;
+  strt=micros();
+    while(toime<3000)
+    {
+      reading=digitalRead(Pulse);
+       if (reading!= lastReading && pock!=1) 
+       {
+       beginning=micros();
+       pock=1;
+       }
+        else if (reading!= lastReading && pock==1) 
+        {
+        ending=micros();
+        haldCycle=ending-beginning;
+        frequency=500000/haldCycle;
+        goto next;
+        }
+        fnsh=micros();
+        toime=fnsh-strt;
+        lastReading=reading;
+    }
 
-start=micros();
-  delay(1000);
+ }
+ 
+ next:
+ 
  finish=micros();
  task=finish-start;
- task=task;
- counter++;
-Serial.print(task,DEC);
- Serial.print('\n');
-
+// tyme=8333-task;
+ //delayMicroseconds(tyme);
+delay(1000);
 }
